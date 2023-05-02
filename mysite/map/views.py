c222934b1ae58e7fe65c02fc271b270f1511ebf2
Bumariam <bumariamnakeeva@gmail.com>
2023-05-02 16:50:50 +0600

    02/05/23

--- mysite/map/views.py
+++ mysite/map/views.py
@@ -1,3 +1,4 @@
+import location as location
 from django.shortcuts import render
 from .models import Location
 import folium
@@ -8,20 +9,13 @@ def home(request):
     locations = Location.objects.all()
     initialMap = folium.Map(location=[41.2611204525265, 74.61809829460819], zoom_start=8)
 
-    lat = [location.lat for location in locations]
-    long = [location.lng for location in locations]
+    data = []
 
-    print(lat)
-    print(long)
-    print(list(zip(lat,long)))
 
-    FastMarkerCluster(data=zip(lat,long)).add_to(initialMap)
+    marker_cluster = FastMarkerCluster(data).add_to(initialMap)
 
- #   for location in locations:
- #       coordinates = (location.lat, location.lng)
- #       folium.Marker(coordinates, popup='Филиал больницы:  ' + location.name).add_to(initialMap)
-
+    for location in locations:
+        folium.Marker(location=[location.lat, location.lng], popup='Филиал больницы: ' + location.name).add_to(marker_cluster)
 
     context = {'map':initialMap._repr_html_(), 'locations':locations}
     return render(request, 'map/home.html', context)
-
