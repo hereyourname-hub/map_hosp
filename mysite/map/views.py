40d3f16b9695e22b16896f216c4baf8b6ce3408e
Bumariam <bumariamnakeeva@gmail.com>
2023-05-02 16:54:19 +0600

    02/05/23 second

--- mysite/map/views.py
+++ mysite/map/views.py
@@ -6,16 +6,15 @@ from folium.plugins import FastMarkerCluster
 
 # Create your views here.
 def home(request):
+    
     locations = Location.objects.all()
     initialMap = folium.Map(location=[41.2611204525265, 74.61809829460819], zoom_start=8)
 
     data = []
-
-
     marker_cluster = FastMarkerCluster(data).add_to(initialMap)
 
     for location in locations:
-        folium.Marker(location=[location.lat, location.lng], popup='Филиал больницы: ' + location.name).add_to(marker_cluster)
+        folium.Marker(location=[location.lat, location.lng], popup='Филиал больницы: ' + location.name + location.address).add_to(marker_cluster)
 
     context = {'map':initialMap._repr_html_(), 'locations':locations}
     return render(request, 'map/home.html', context)
